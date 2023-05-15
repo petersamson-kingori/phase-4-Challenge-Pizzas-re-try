ddc82b8dea87c290065e1c9b17872cc2413e2d09
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-05-15 17:17:51 +0300

    complete

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,4 +13,16 @@ class RestaurantsController < ApplicationController
           render json: { error: "Restaurant not found" }, status: :not_found
         end
       end
+
+      def destroy
+        restaurant = Restaurant.find_by(id: params[:id])
+    
+        if restaurant
+          restaurant.restaurant_pizzas.destroy_all
+          restaurant.destroy
+          head :no_content
+        else
+          render json: { error: "Restaurant not found" }, status: :not_found
+        end
+      end
   end
